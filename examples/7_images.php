d37f91f286c36e36997b995be84d7d12875cbc1e
Holger McCloy <holger.mccloy@netzleuchten.com>
2022-05-10 17:12:51 +0200

    [tasl] Added example for using image meta data

--- examples/7_images.php
+++ examples/7_images.php
@@ -9,6 +9,15 @@ $repository = $api->factory(\Nordkirche\Ndk\Domain\Repository\InstitutionReposit
 /** @var \Nordkirche\Ndk\Domain\Model\Institution\Institution $instituion */
 $instituion = $repository->getById(1930);
 
+/** @var string $imageUrl */
 $imageUrl = $instituion->getLogo()->render(150);
 
+/** @var \Nordkirche\Ndk\Domain\Model\File\Details $imageDetails */
+$imageDetails = $instituion->getLogo()->getDetails();
+
+/** @var string $copyright */
+$copyright = $imageDetails->getCopyright();
+
 echo $imageUrl . PHP_EOL;
+
+echo $copyright . PHP_EOL;
